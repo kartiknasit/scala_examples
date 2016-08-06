2fedcd6795f00b06a273c7c8cec4d53ac3906c68
Kartik Patel <kartiknasit@gmail.com>
2016-08-06 11:36:07 +0530

    Scala Example with for loop printing A-Z letters easily

---
+++ ScalaTutorial2.scala
@@ -0,0 +1,8 @@
+object ScalaTutorial2 {
+  def main(args:Array[String]){
+    val letters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+    for(i<-0 until letters.length()){
+      println(letters(i))
+    }
+  }
+}
